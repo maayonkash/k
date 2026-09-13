b6770763a0f1c24b312cdfaf67ddc70b91d70c71
maayonkash <maayonkash@gmail.com>
2026-09-13 13:42:52 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+    radio.sendNumber(2)
+    radio.sendNumber(3)
+    radio.sendNumber(4)
+    radio.sendNumber(5)
 })
 radio.setGroup(1)
 basic.forever(function () {
